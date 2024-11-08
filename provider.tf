e8eeac6f46760fcfec9c9bfafc4d1c0b1e9ec93b
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2024-11-08 16:01:05 -0300

    feat: create vpc for eks project and create bucket s3 for the save of file .tfstate

--- provider.tf
+++ provider.tf
@@ -1,14 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.75.0"  
+      source  = "hashicorp/aws"
+      version = "5.75.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "eksdevops-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
 
 
