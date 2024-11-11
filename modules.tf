535ed967fac450bc6c4e5c4a06810b666ba39691
Bandeira Magalhães <bandeiramagalhaesoficial@gmail.com>
2024-11-11 12:30:20 -0300

    refactor: add variable oidc

--- modules.tf
+++ modules.tf
@@ -26,4 +26,5 @@ module "eks_load_balancer_controller" {
   source       = "./modules/load-balancer-controller"
   project_name = var.project_name
   tags         = local.tags
+  oidc         = module.eks_cluster.oidc
 }
